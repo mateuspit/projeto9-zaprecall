c91485d6d1bfe061f87c6513a47929e99694bd6e
Mateus Barcelos <mateuspit@hotmail.com>
2023-03-03 12:34:41 -0300

    homepage styled components done, next questions page

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import React from "react";
+import Logo from "../assets/logo.png"
+
+export default function HomePage() {
+    return (
+        <ContainerHomePage>
+            <LogoHome src={Logo} alt="Logo" />
+            <TitleHome>ZapRecall</TitleHome>
+            <ButtonHome>Iniciar Recall!</ButtonHome>
+        </ContainerHomePage>
+    );
+}
+
+const ContainerHomePage = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`;
+
+const LogoHome = styled.img`
+    width: 136px;
+    margin-top: 148px;
+`;
+
+const TitleHome = styled.h1`
+    color: white;
+    font-size: 36px;
+    font-family: 'Righteous', cursive;
+    margin-top: 13px;
+    margin-bottom: 30px;
+`;
+
+const ButtonHome = styled.button`
+    width: 246px;
+    height: 54px;
+    color: #D70900;
+    background-color: #ffffff;
+    border-radius: 5px;
+    border: solid 1px #D70900;
+    font-family: 'Recursive', sans-serif;
+    font-weight: 400;
+    font-size: 18px;
+    text-align: center;
+`;
