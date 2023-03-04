f86c0e26580aaa84d6d4b3457c3619346eb0bdd7
Mateus Barcelos <mateuspit@hotmail.com>
2023-03-04 19:29:22 -0300

    Card question board finish, next answer board

--- src/components/QuestionSpot.jsx
+++ src/components/QuestionSpot.jsx
@@ -5,10 +5,17 @@ import QuestionNumberSpot from "./QuestionNumberSpot";
 import Wrong from "../assets/icone_erro.png"
 import Right from "../assets/icone_certo.png"
 import Almost from "../assets/icone_quase.png"
+import TurnArrowIcon from "../assets/seta_virar.png"
 
 export default function QuestionSpot() {
     return (
-        <ContainerQuestionSpot></ContainerQuestionSpot>
+        <ContainerQuestionSpot>
+            <QuestionText>
+            O que é JSX?aO queque é JSX?aO que é JSX?aO que é JSX?a
+            </QuestionText>
+            <ShowAnswerButton src={TurnArrowIcon} alt={"Botão de virar"}/>
+
+        </ContainerQuestionSpot>
     );
 }
 
@@ -16,9 +23,28 @@ export default function QuestionSpot() {
 
 const ContainerQuestionSpot = styled.div`
     width: 299px;
-    height: 131px;
+    min-height: 131px;
     background: #FFFFD5;
     box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
     border-radius: 5px;
     margin-top: 25px;
+    position: relative;
+`;
+
+const QuestionText = styled.p`
+    margin-top: 18px;
+    margin-left: 15px;
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    color: #333333;
+`;
+
+const ShowAnswerButton = styled.img`
+    width: 30px;
+    position: absolute;
+    bottom: 6px;
+    right: 15px;
 `;
